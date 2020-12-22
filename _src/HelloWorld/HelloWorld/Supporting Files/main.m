1eb06d230afb29c90036343e2101dc7e6c786cd0
Paul Lewallen <paullewallencom@users.noreply.github.com>
2020-12-22 12:48:42 -0800

    Deleting extraneous .git
    Deleting extraneous .git directory.

---
+++ _src/HelloWorld/HelloWorld/Supporting Files/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  HelloWorld
+//
+//  Created by Ben Trengrove on 25/05/2014.
+//  Copyright Ben Trengrove 2014. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+int main(int argc, char *argv[]) {
+    
+    @autoreleasepool {
+        int retVal = UIApplicationMain(argc, argv, nil, @"AppDelegate");
+        return retVal;
+    }
+}
